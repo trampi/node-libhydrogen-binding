cdf5fc8077d9aa71a0ba7b60aac9b1cb97bcd584
Fabian Beier-Trampusch <fabian.trampusch@freenet.de>
2018-05-17 22:45:16 +0200

    added binding for probes

--- binding.cc
+++ binding.cc
@@ -40,12 +40,20 @@ NAN_METHOD(_hydro_secretbox_keygen) {
   info.GetReturnValue().Set(result);
 }
 
-NAN_GETTER(_hydro_secretbox_KEYBYTES) {
-  info.GetReturnValue().Set(hydro_secretbox_KEYBYTES);
+NAN_GETTER(_hydro_secretbox_CONTEXTBYTES) {
+  info.GetReturnValue().Set(hydro_secretbox_CONTEXTBYTES);
 }
 
 NAN_GETTER(_hydro_secretbox_HEADERBYTES) {
   info.GetReturnValue().Set(hydro_secretbox_HEADERBYTES);
+}
+
+NAN_GETTER(_hydro_secretbox_KEYBYTES) {
+  info.GetReturnValue().Set(hydro_secretbox_KEYBYTES);
+}
+
+NAN_GETTER(_hydro_secretbox_PROBEBYTES) {
+  info.GetReturnValue().Set(hydro_secretbox_PROBEBYTES);
 }
 
 NAN_METHOD(_hydro_secretbox_encrypt) {
@@ -93,6 +101,7 @@ NAN_METHOD(_hydro_secretbox_encrypt) {
   if (keyBuffer.length() != hydro_secretbox_KEYBYTES) {
     isolate->ThrowException(Nan::TypeError(
           String::NewFromUtf8(isolate, "illegal key, size mismatch")));
+    return;
   }
 
   uint64_t msgid = info[2]->NumberValue();
@@ -160,6 +169,7 @@ NAN_METHOD(_hydro_secretbox_decrypt) {
   if (keyBuffer.length() != hydro_secretbox_KEYBYTES) {
     isolate->ThrowException(Nan::TypeError(
           String::NewFromUtf8(isolate, "illegal key, size mismatch")));
+    return;
   }
 
   uint64_t msgid = info[2]->NumberValue();
@@ -174,17 +184,148 @@ NAN_METHOD(_hydro_secretbox_decrypt) {
 
   size_t decrypted_len = ciphertext.length() - hydro_secretbox_HEADERBYTES;
   uint8_t * decrypted = new uint8_t[decrypted_len + 1];
-
-  if (hydro_secretbox_decrypt(decrypted, *ciphertext, ciphertext.length(), msgid, context.c_str(), *keyBuffer) != 0) {
-      isolate->ThrowException(Nan::TypeError(
-            String::NewFromUtf8(isolate, "message forged")));
-      return;
-  }
-
+  int decryption_result = hydro_secretbox_decrypt(decrypted, *ciphertext, ciphertext.length(), msgid, context.c_str(), *keyBuffer);
   std::string resultstr((char*) decrypted, decrypted_len);
   delete decrypted;
 
+  if (decryption_result != 0) {
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, "message forged")));
+    return;
+  }
+
   info.GetReturnValue().Set(String::NewFromUtf8(isolate, resultstr.c_str()));
+}
+
+NAN_METHOD(_hydro_secretbox_probe_create) {
+  Isolate* isolate = info.GetIsolate();
+
+  // ciphertext, context, key
+
+  if (info.Length() != 3) {
+    isolate->ThrowException(Nan::TypeError(
+        String::NewFromUtf8(isolate, "Wrong number of arguments, expected 3")));
+    return;
+  }
+
+  if (!info[0]->IsUint8Array()) {
+    const char* error = std::string("expected ciphertext to be a Uint8Array, got '" + get_constructor_name(info[0]) + "'").c_str();
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, error)));
+    return;
+  }
+
+  if (!info[1]->IsString()) {
+    const char* error = std::string("expected context to be a string, got '" + get_constructor_name(info[1]) + "'").c_str();
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, error)));
+    return;
+  }
+
+  if (!info[2]->IsUint8Array()) {
+    const char* error = std::string("expected key to be a Uint8Array, got '" + get_constructor_name(info[1]) + "'").c_str();
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, error)));
+    return;
+  }
+
+  Local<Uint8Array> ciphertextLocal = info[0].As<Uint8Array>();
+  Nan::TypedArrayContents<uint8_t> ciphertext(ciphertextLocal);
+
+  std::string context(local_string_to_string(info[1]->ToString()));
+  if (context.length() != hydro_secretbox_CONTEXTBYTES) {
+    const char* error = std::string("invalid context length").c_str();
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, error)));
+    return;
+  }
+
+  Local<Uint8Array> keyLocal = info[2].As<Uint8Array>();
+  Nan::TypedArrayContents<uint8_t> keyBuffer(keyLocal);
+  if (keyBuffer.length() != hydro_secretbox_KEYBYTES) {
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, "illegal key, size mismatch")));
+    return;
+  }
+
+  uint8_t * probe = new uint8_t[hydro_secretbox_PROBEBYTES];
+  hydro_secretbox_probe_create(probe, *ciphertext, ciphertext.length(), context.c_str(), *keyBuffer);
+  auto result = Nan::NewBuffer((char*) probe, hydro_secretbox_PROBEBYTES).ToLocalChecked();
+  info.GetReturnValue().Set(result);
+}
+
+NAN_METHOD(_hydro_secretbox_probe_verify) {
+  Isolate* isolate = info.GetIsolate();
+
+  // probe, ciphertext, context, key
+
+  if (info.Length() != 4) {
+    isolate->ThrowException(Nan::TypeError(
+        String::NewFromUtf8(isolate, "Wrong number of arguments, expected 4")));
+    return;
+  }
+
+  if (!info[0]->IsUint8Array()) {
+    const char* error = std::string("expected probe to be a Uint8Array, got '" + get_constructor_name(info[0]) + "'").c_str();
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, error)));
+    return;
+  }
+
+  if (!info[1]->IsUint8Array()) {
+    const char* error = std::string("expected ciphertext to be a Uint8Array, got '" + get_constructor_name(info[1]) + "'").c_str();
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, error)));
+    return;
+  }
+
+  if (!info[2]->IsString()) {
+    const char* error = std::string("expected context to be a string, got '" + get_constructor_name(info[1]) + "'").c_str();
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, error)));
+    return;
+  }
+
+  if (!info[3]->IsUint8Array()) {
+    const char* error = std::string("expected key to be a Uint8Array, got '" + get_constructor_name(info[1]) + "'").c_str();
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, error)));
+    return;
+  }
+
+  Local<Uint8Array> probeLocal = info[0].As<Uint8Array>();
+  Nan::TypedArrayContents<uint8_t> probe(probeLocal);
+  if (probe.length() != hydro_secretbox_PROBEBYTES) {
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, "illegal probe, size mismatch")));
+          return;
+  }
+
+  Local<Uint8Array> ciphertextLocal = info[1].As<Uint8Array>();
+  Nan::TypedArrayContents<uint8_t> ciphertext(ciphertextLocal);
+
+  std::string context(local_string_to_string(info[2]->ToString()));
+  if (context.length() != hydro_secretbox_CONTEXTBYTES) {
+    const char* error = std::string("invalid context length").c_str();
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, error)));
+    return;
+  }
+
+  Local<Uint8Array> keyLocal = info[3].As<Uint8Array>();
+  Nan::TypedArrayContents<uint8_t> keyBuffer(keyLocal);
+  if (keyBuffer.length() != hydro_secretbox_KEYBYTES) {
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, "illegal key, size mismatch")));
+    return;
+  }
+
+  if (hydro_secretbox_probe_verify(*probe, *ciphertext, ciphertext.length(), context.c_str(), *keyBuffer) != 0) {
+    const char* error = std::string("probe verify failed").c_str();
+    isolate->ThrowException(Nan::TypeError(
+          String::NewFromUtf8(isolate, error)));
+    return;
+  }
 }
 
 NAN_MODULE_INIT(Initialize) {
@@ -192,10 +333,17 @@ NAN_MODULE_INIT(Initialize) {
   Nan::Export(target, "random_u32", _hydro_random_u32);
   Nan::Export(target, "random_uniform", _hydro_random_uniform);
   Nan::Export(target, "secretbox_keygen", _hydro_secretbox_keygen);
-  Nan::SetAccessor(target, Nan::New("secretbox_keybytes_size").ToLocalChecked(), _hydro_secretbox_KEYBYTES);
+
+  Nan::SetAccessor(target, Nan::New("secretbox_contextbytes_size").ToLocalChecked(), _hydro_secretbox_CONTEXTBYTES);
   Nan::SetAccessor(target, Nan::New("secretbox_headerbytes_size").ToLocalChecked(), _hydro_secretbox_HEADERBYTES);
+  Nan::SetAccessor(target, Nan::New("secretbox_keybytes_size").ToLocalChecked(), _hydro_secretbox_KEYBYTES);
+  Nan::SetAccessor(target, Nan::New("secretbox_probebytes_size").ToLocalChecked(), _hydro_secretbox_PROBEBYTES);
+
   Nan::Export(target, "secretbox_encrypt", _hydro_secretbox_encrypt);
   Nan::Export(target, "secretbox_decrypt", _hydro_secretbox_decrypt);
+
+  Nan::Export(target, "secretbox_probe_create", _hydro_secretbox_probe_create);
+  Nan::Export(target, "secretbox_probe_verify", _hydro_secretbox_probe_verify);
 }
 
 NODE_MODULE(libhydrogennative, Initialize)
--- test/tests.js
+++ test/tests.js
@@ -57,44 +57,95 @@ describe('hydrogen.secretbox_keygen', () => {
 
 });
 
-describe('hydrogen.secretbox_encrypt', () => {
+describe('secret key encryption', () => {
 
     const msg = "message";
     const key = hydrogen.secretbox_keygen();
     const msgId = 0;
     const context = "testtest";
+    const invalidContext = "invalid context";
+    const keyWithInvalidSize = Buffer.alloc(5);
 
-    it('should expose the constant for keybytes length', () => {
-        expect(hydrogen.secretbox_keybytes_size).to.eq(32);
+    describe('hydrogen.secretbox_encrypt', () => {
+
+        it('should expose the constant for contextbytes length', () => {
+            expect(hydrogen.secretbox_contextbytes_size).to.eq(8);
+        });
+
+        it('should expose the constant for keybytes length', () => {
+            expect(hydrogen.secretbox_keybytes_size).to.eq(32);
+        });
+
+        it('should expose the constant for headerbytes length', () => {
+            expect(hydrogen.secretbox_headerbytes_size).to.eq(36);
+        });
+
+        it('should encrypt something', () => {
+            const ciphertext = hydrogen.secretbox_encrypt(msg, key, msgId, context);
+            expect(ciphertext).to.be.instanceof(Buffer);
+            expect(ciphertext.length).to.be.equal(43);
+        });
+
+        it('should decrypt encrypted messages correctly', () => {
+            const ciphertext = hydrogen.secretbox_encrypt(msg, key, msgId, context);
+            expect(hydrogen.secretbox_decrypt(ciphertext, key, msgId, context)).to.equal(msg);
+        });
+
+        it('should detect forged messages', () => {
+            const ciphertext = hydrogen.secretbox_encrypt(msg, key, msgId, context);
+            ciphertext.set([1, 2, 3], 3);
+            expect(() => hydrogen.secretbox_decrypt(ciphertext, key, msgId, context)).to.throw();
+        });
+
+        it('should detect invalid context lengths', () => {
+            expect(() => hydrogen.secretbox_encrypt(msg, key, msgId, invalidContext)).to.throw();
+        });
+
+        it('should detect invalid keys', () => {
+            expect(() => hydrogen.secretbox_encrypt(msg, new Uint8Array(5), msgId, "test")).to.throw();
+        });
+
     });
 
-    it('should expose the constant for headerbytes length', () => {
-        expect(hydrogen.secretbox_headerbytes_size).to.eq(36);
-    });
+    describe('hydrogen.secretbox_probe', () => {
 
-    it('should encrypt something', () => {
         const ciphertext = hydrogen.secretbox_encrypt(msg, key, msgId, context);
-        expect(ciphertext).to.be.instanceof(Buffer);
-        expect(ciphertext.length).to.be.equal(43);
-    });
+        const probe = hydrogen.secretbox_probe_create(ciphertext, context, key);
+        const probeWithInvalidSize = Buffer.alloc(5);
 
-    it('should decrypt encrypted messages correctly', () => {
-        const ciphertext = hydrogen.secretbox_encrypt(msg, key, msgId, context);
-        expect(hydrogen.secretbox_decrypt(ciphertext, key, msgId, context)).to.equal(msg);
-    });
+        it('should expose the constant for probebytes length', () => {
+            expect(hydrogen.secretbox_probebytes_size).to.eq(16);
+        });
 
-    it('should detect forged messages', () => {
-        const ciphertext = hydrogen.secretbox_encrypt(msg, key, msgId, context);
-        ciphertext.set([1,2,3], 3);
-        expect(() => hydrogen.secretbox_decrypt(ciphertext, key, msgId, context)).to.throw();
-    });
+        it('should generate and verify probes', () => {
+            expect(() => hydrogen.secretbox_probe_verify(probe, ciphertext, context, key)).not.to.throw();
+        });
 
-    it('should detect invalid context lengths', () => {
-        expect(() => hydrogen.secretbox_encrypt(msg, key, msgId, "test")).to.throw();
-    });
+        it('should throw on invalid probes', () => {
+            const probe = new Uint8Array(hydrogen.secretbox_probebytes_size);
+            expect(() => hydrogen.secretbox_probe_verify(probe, ciphertext, context, key)).to.throw();
+        });
 
-    it('should detect invalid keys', () => {
-        expect(() => hydrogen.secretbox_encrypt(msg, new Uint8Array(5), msgId, "test")).to.throw();
+        it('should throw on invalid context size when creating a probe', () => {
+            expect(() => hydrogen.secretbox_probe_create(ciphertext, invalidContext, key)).to.throw();
+        });
+
+        it('should throw on invalid key size when creating a probe', () => {
+            expect(() => hydrogen.secretbox_probe_create(ciphertext, context, keyWithInvalidSize)).to.throw();
+        });
+
+        it('should throw on invalid probe size when verifying a probe', () => {
+            expect(() => hydrogen.secretbox_probe_verify(probeWithInvalidSize, ciphertext, context, key)).to.throw();
+        });
+
+        it('should throw on invalid context size when verifying a probe', () => {
+            expect(() => hydrogen.secretbox_probe_verify(probe, ciphertext, invalidContext, key)).to.throw();
+        });
+
+        it('should throw on invalid key size when verifying a probe', () => {
+            expect(() => hydrogen.secretbox_probe_verify(probe, ciphertext, context, keyWithInvalidSize)).to.throw();
+        });
+
     });
 
 });
